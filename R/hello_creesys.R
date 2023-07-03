6027c1b88677296f7b1d854680549ac11025a0ff
J. Holden <jeremy.holden@ontario.ca>
2023-07-03 10:56:09 -0400

    updates hello creesys function

--- R/hello_creesys.R
+++ R/hello_creesys.R
@@ -1,11 +1,14 @@
 #' hello creesys
 #'
-#' @return str "I only say hello creesys, but that's cool."
+#' @return str "hello creesys"
 #' @export
+#' @description
+#' This function only returns 'hello creesys' but
+#' is here as a simple example of how to write functions.
 #'
 #' @examples
 #' hello_creesys()
 
 hello_creesys <- function(){
-  "I only say hello creesys, but that's cool."
+  "hello creesys"
 }
