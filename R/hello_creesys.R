be9664e51c992ca44fe22a89212c6ca7026748bd
J. Holden <jeremy.holden@ontario.ca>
2023-07-03 10:10:22 -0400

    update hello_creesys

--- R/hello_creesys.R
+++ R/hello_creesys.R
@@ -1,11 +1,11 @@
 #' hello creesys
 #'
-#' @return str "hello creesys"
+#' @return str "I only say hello creesys, but that's cool."
 #' @export
 #'
 #' @examples
 #' hello_creesys()
 
 hello_creesys <- function(){
-  "I only say hello creesys"
+  "I only say hello creesys, but that's cool."
 }
