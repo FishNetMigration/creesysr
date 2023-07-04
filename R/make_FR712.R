b0c28c54263d9669e2b08cebba7d0588a0fb3991
J. Holden <jeremy.holden@ontario.ca>
2023-07-04 12:06:25 -0400

    initial function commit

---
+++ R/make_FR712.R
@@ -0,0 +1,14 @@
+#' make_FR712
+#' @description
+#' This function takes FN* tables as inputs and generates the FR712 table.
+#'
+#' @param fntables
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+make_FR712 <- function(fntables) {
+  print("this function makes the FR712")
+}
