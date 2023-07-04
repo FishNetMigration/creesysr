4ca98c8ac963c97029e534aabbbb0429a8d7830b
J. Holden <jeremy.holden@ontario.ca>
2023-07-04 13:58:02 -0400

    introduces parse_STRAT

---
+++ R/parse_STRAT.R
@@ -0,0 +1,37 @@
+#' parse_STRAT
+#' @description
+#' STRAT is an important grouping variable used throughout FN2. Often though
+#' it is necessary to have the individual components parsed out in to data columns
+#' in order to be able to join tables together.
+#'
+#'
+#' @param fndf a FN2 data table that includes STRAT as a column
+#'
+#' @return return the same FN2 dataframe with SSN, DTP, PRD, SPACE, and MODE columns
+#' @export
+#'
+#' @examples
+#'
+parse_STRAT <- function(fndf) {
+  # fndf can be any FN2 table as long as it has STRAT
+  if(!(STRAT %in% names(fndf))) {stop("STRAT is not a column in this dataframe")}
+
+  # Season
+  fndf$SSN <- substr(fndf$STRAT, 1, 2)
+
+  # Day type
+  fndf$DTP <- substr(fndf$STRAT, 4, 4)
+
+  # Period
+  fndf$PRD <- substr(fndf$STRAT, 5,5)
+
+  # Space
+  fndf$SPACE <- substr(fndf$STRAT, 7, 8)
+
+  # Mode
+  fndf$MODE <- substr(fndf$STRAT, 10, 11)
+
+  # Return new appended df
+  fndf
+
+}
