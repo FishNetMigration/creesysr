68d3d3730919aca826a0e3e98dd2e77e8eb42e96
Victoria Danco <60553399+victoriadancoMNRF@users.noreply.github.com>
2023-07-05 11:54:05 -0500

    prep for make_FR713 function
    connected to issue #14

---
+++ R/make_FR713.R
@@ -0,0 +1,26 @@
+#' make_FR713
+#'
+#' @param fn011 FN project definition table FN011 which lists project dates, year, lake name and project leader information
+#' @param fn111 FN portion of the CLF that oulines the SAMA, Date, DOW, STRATUM,
+#' @param fn112 FN portion of the CLF that outlines the activity count, time of the count and number of interviews that occured. Note: multiple counts could occur per SAMA
+#' @param fn121 FN CIF creel interview form
+#' @param fn024 FN stratum definition for period i.e. am or pm
+#'
+#' @description
+#' This is the creel estimate table for EFFORT
+#'
+#'
+#' @return
+#' @export
+#'
+#' @examples
+make_FR713<-function(fn011, fn111, fn112, fn121, fn024){
+
+  # create daily estimate FR713_daily
+
+
+
+}
+
+
+
