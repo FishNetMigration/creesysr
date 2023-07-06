053e7238fb8d9bc77c253f2c223ba027792def00
Victoria Danco <60553399+victoriadancoMNRF@users.noreply.github.com>
2023-07-06 14:12:12 -0500

    add calculation for STRAT_HRS based on space and mode

--- R/make_FR713.R
+++ R/make_FR713.R
@@ -23,8 +23,8 @@ make_FR713<-function(fn011, fn022, fn023, fn024, fn025, fn026, fn028, fn111, fn1
 
   # create daily estimate FR713_daily
 
-  # cSPACE<-fn026 %>% group_by(PRJ_CD) %>% dplyr::summarize(cSPACE=n())
-  # cMODE<-fn028 %>% group_by(PRJ_CD) %>% dplyr::summarize(cMODE=n())
+  cSPACE<-fn026 %>% group_by(PRJ_CD) %>% dplyr::summarize(cSPACE=n())
+  cMODE<-fn028 %>% group_by(PRJ_CD) %>% dplyr::summarize(cMODE=n())
 
 
   CreelData111_112<-left_join(fn111, fn112, by=c("SAMA", "PRJ_CD"))
@@ -310,9 +310,12 @@ make_FR713<-function(fn011, fn022, fn023, fn024, fn025, fn026, fn028, fn111, fn1
       MODE = as.numeric(substr(STRATUM, 10, 11))
     ) %>%
 
+    left_join(., cSPACE, by=c("PRJ_CD")) %>%
+    left_join(., cMODE, by=c("PRJ_CD")) %>%
+
     group_by(PRJ_CD, AREA) %>%
     dplyr::summarize( SAM_DAYS=sum(SAM_DAYS_h, na.rm=TRUE),
-                      STRAT_HRS=sum(as.numeric(STRAT_HRS_h))/6, # count of space is 4
+                      STRAT_HRS=sum(as.numeric(STRAT_HRS_h))/cSPACE/cMODE,
                       #STRAT_HRS: Sum([STRAT_HRS_h])/IIf([Forms]![R Creel Estimates]![cbo_SPACE] Is Null,First([cSPACE]),1)/IIf([Forms]![R Creel Estimates]![cbo_MODE] Is Null,First([cMODE]),1)
                       ATY_NN=sum(ATY_NN_h, na.rm=TRUE),
                       ATYCNT=sum(ATYCNT_h, na.rm=TRUE),
